69b9b5411ad969012c4a05b80227e34f0b0550e7
Rico <enriquelopez80@gmail.com>
2018-07-21 21:53:56 -0700

    liri push

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # liri-node-app
+
+The Liri node app takes commands and outputs information.
+
+Type in 'node liri.js my-tweets' and get my last 20 tweets
+
+Type in node liri.js spotify-this-song '<song name here>' and get:
+    1. Artist
+    2. Song Name
+    3. Preview Link for Song From Spotify
+    4. Album
+
+Type in node liri.js movie-this '<movie name here>' and get:
+    1. Title of the movie.
+    2. Year the movie came out.
+    3. IMDB Rating of the movie.
+    4. Rotten Tomatoes Rating of the movie.
+    5. Country where the movie was produced.
+    6. Language of the movie.
+    7. Plot of the movie.
+    8. Actors in the movie.
+
+Type in node liri.js do-what-it-says and get the response from the random.txt file
+
+I installed 3 packages to run:
+
+npm install twitter
+npm install spotify
+npm install request
--- liri.js
+++ liri.js
@@ -12,110 +12,117 @@ let client = new Twitter(keys.twitter);
 let command = process.argv[2];
 let input = process.argv[3];
 
-//displaying my tweets
+//switch statements
+
 switch (command) {
     case 'my-tweets':
         myTweets();
         break;
-    default:
-        console.log("NOT RIGHT");
-}
-
-function myTweets () {
-    let params = {
-    screen_name: 'enriquealopez',
-    count: 20
-        };
-    client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (error) {
-        console.log("error" + error);
-        }
-        else {
-            tweets.forEach(function (tweets) {
-                console.log(`Tweet: ${tweets.text}`)
-                console.log(`Created on: ${tweets.created_at}`)
-            });
-        }
-    });
-}
-
-
-//spotify this song
-switch (command) {
     case 'spotify-this-song':
         mySong();
         break;
-    default:
-        console.log("NOT RIGHT");
-}
-
-function mySong () {
-spotify.search({ type: 'track', query: input }, function(err, data, response) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   console.log(`Artist: ${data.tracks.items[0].artists[0].name}`)
-   console.log(`Song: ${data.tracks.items[0].name}`)
-   console.log(`Preview: ${data.tracks.items[0].album.external_urls.spotify}`)
-   console.log(`Album: ${data.tracks.items[0].album.name}`)
-  });
-}
-
-//movie this
-
-switch (command) {
     case 'movie-this':
         myMovie();
+        break;
+    case 'do-what-it-says':
+        doIt()
         break;
     default:
         console.log("NOT RIGHT");
 }
 
+//my tweets function
 
-function myMovie () {
+function myTweets() {
+    let params = {
+        screen_name: 'enriquealopez',
+        count: 20
+    };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (error) {
+            console.log("error" + error);
+        } else {
+            tweets.forEach(function (tweets) {
+                console.log(`Tweet: ${tweets.text}`)
+                console.log(`Created on: ${tweets.created_at}`)
+                //append to the log
+                fs.appendFile("log.txt", JSON.stringify(tweets.text + tweets.created_at, null, 2), function (error) {
+                    if (error) {
+                        console.log(error);
+                    }
+                });
+            });
+
+        }
+    });
+
+}
+
+//my song function
+
+function mySong() {
+
+    if (!input) {
+        input = 'The Sign'
+    }
+
+    spotify.search({
+        type: 'track',
+        query: input
+    }, function (err, data, response) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(`Artist: ${data.tracks.items[0].artists[0].name}`)
+        console.log(`Song: ${data.tracks.items[0].name}`)
+        console.log(`Preview: ${data.tracks.items[0].album.external_urls.spotify}`)
+        console.log(`Album: ${data.tracks.items[0].album.name}`)
+    });
+}
+
+//movie this function
+
+function myMovie() {
+
+    if (!input) {
+        input = 'Mr. Nobody'
+    }
 
     let queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
 
-    request(queryUrl, function(error, response, body) {
+    request(queryUrl, function (error, response, body) {
         if (error) {
             return console.log('Error:' + error)
         }
-        console.log ('Title: ' + JSON.parse(body).Title)
-        console.log ('Year Released: ' + JSON.parse(body).Year)
-        console.log ('IMBD Rating: ' + JSON.parse(body).Ratings[0].Value)
-        console.log ('RT Rating: ' + JSON.parse(body).Ratings[1].Value)
-        console.log ('Country: ' + JSON.parse(body).Country)
-        console.log ('Language: ' + JSON.parse(body).Language)
-        console.log ('Plot: ' + JSON.parse(body).Plot)
-        console.log ('Actors: ' + JSON.parse(body).Actors)
-        
+        console.log('Title: ' + JSON.parse(body).Title)
+        console.log('Year Released: ' + JSON.parse(body).Year)
+        console.log('IMBD Rating: ' + JSON.parse(body).Ratings[0].Value)
+        console.log('RT Rating: ' + JSON.parse(body).Ratings[1].Value)
+        console.log('Country: ' + JSON.parse(body).Country)
+        console.log('Language: ' + JSON.parse(body).Language)
+        console.log('Plot: ' + JSON.parse(body).Plot)
+        console.log('Actors: ' + JSON.parse(body).Actors)
+
     });
 }
 
 
 //do what it says
 
-fs.readFile("random.txt", "utf8", function(error, data) {
-    if (error) {
-        return console.log(error);
-      }
-      let dataArr = data.split(",");
-
-      switch (command) {
-        case dataArr[0]:
-            mySong();
-            break;
-        default:
-            console.log("NOT RIGHT");
-    }
-      
-      spotify.search({ type: 'track', query: dataArr[1] }, function(err, data, response) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
+function doIt() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
         }
-       console.log(`Artist: ${data.tracks.items[0].artists[0].name}`)
-       console.log(`Song: ${data.tracks.items[0].name}`)
-       console.log(`Preview: ${data.tracks.items[0].album.external_urls.spotify}`)
-       console.log(`Album: ${data.tracks.items[0].album.name}`)
-      });
-});
+            dataArr = data.split(",");
+            //console.log(dataArr[0])
+            //console.log(dataArr[1])
+
+            mySong(dataArr[0], dataArr[1]);
+            //myTweets(dataArr[0])
+            //myMovie(dataArr[0],dataArr[1])
+
+            //the function will work for each commmand when uncommented
+                   
+    })
+}
